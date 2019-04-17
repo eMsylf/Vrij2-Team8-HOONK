3a172ada4a18318f8c6ec882487f4b4f30a4aa11
Bob <bobjeltes@hotmail.com>
2019-04-17 09:50:07 +0200

    Making player movement

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody rigidbody_Player;
+    private Quaternion rigidbodyQuaternion;
+
+    void Start() {
+        rigidbody_Player = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        if (Input.GetAxis("Horizontal") < 0) {
+            //move left
+            rigidbodyQuaternion = new Quaternion(0, -90, 0, 0);
+        }
+        else if (Input.GetAxis("Horizontal") > 0) {
+            //move right
+            rigidbodyQuaternion = new Quaternion(0, 90, 0, 0);
+        }
+        //rigidbodyQuaternion = new Quaternion(0, Input.GetAxis("Horizontal"), 0, 0);
+        rigidbody_Player.MoveRotation(rigidbodyQuaternion);
+    }
+}
