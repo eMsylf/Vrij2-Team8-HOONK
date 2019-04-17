97e3e0740f494fea66253ca5e7c12a38f503f4e8
Bob <bobjeltes@hotmail.com>
2019-04-17 14:39:06 +0200

    First playable
    Movement working
    Hit detection &sight working half
    Turning enemy
    End goal

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/Eye.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour {
+    public float ViewDistance = 5;
+
+    void FixedUpdate() {
+        CastRays();
+    }
+
+    private void CastRays() {
+        Physics.Raycast(transform.position, transform.up, out RaycastHit hitInfo, ViewDistance);
+        if (hitInfo.transform == null) {
+            return;
+        } else { 
+            Debug.Log("Hit " + hitInfo.transform.name);
+        }
+        /*
+        if (hitInfo.transform.name == "Player") {
+            Debug.Log("Hit " + hitInfo.transform.name);
+        }
+        */
+    }
+}
