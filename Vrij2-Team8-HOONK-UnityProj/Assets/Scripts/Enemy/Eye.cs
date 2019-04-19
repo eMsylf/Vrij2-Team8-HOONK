983539d62343b401e2a37df005d0b2663e6014c0
Bob Jeltes <bobjeltes@hotmail.com>
2019-04-19 17:27:39 +0200

    Working on enemy's movement detection
    I got an easy to use detection cone/radius tool working to some extent but it needs more work.

--- Vrij2-Team8-HOONK-UnityProj/Assets/Scripts/Enemy/Eye.cs
+++ Vrij2-Team8-HOONK-UnityProj/Assets/Scripts/Enemy/Eye.cs
@@ -2,15 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Eye : MonoBehaviour {
-    public float ViewDistance = 5;
+    [Range(1, 100)]
+    public int NumberOfDetectionLines = 3;
+    [Range(0, 10)]
+    public float ViewDistance = 5.0f;
+    [Range(0, 360)]
+    public float ViewAngle = 15.0f;
 
-    void FixedUpdate() {
+    private float viewDiameter;
+    private Vector3 drawLineTo;
+    private LineRenderer lineRenderer;
+
+    private void Awake() {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+        //lineRenderer.useWorldSpace = true;
+    }
+
+    private void FixedUpdate() {
         CastRays();
     }
 
     private void CastRays() {
-        Physics.Raycast(transform.position, transform.up, out RaycastHit hitInfo, ViewDistance);
+        if (NumberOfDetectionLines == 1) { // 1 detection line
+            DrawDetectionLines(0);
+            ForLoopStuff();
+        } else if (NumberOfDetectionLines % 2 == 0) { // EVEN number of detection lines
+            for (float i = -NumberOfDetectionLines / 2; i <= NumberOfDetectionLines / 2; i++) {
+                DrawDetectionLines(i);
+                ForLoopStuff();
+            }
+        } else { // UNEVEN number of detection lines
+            for (float i = -NumberOfDetectionLines / 2 - .5f; i <= NumberOfDetectionLines / 2 + .5f; i++) {
+                DrawDetectionLines(i);
+                ForLoopStuff();
+            }
+        }
+    }
+
+    private void ForLoopStuff() {
+    }
+
+    private void RayVisualisation () {
+
+    }
+
+    private void OnDrawGizmos() {
+        viewDiameter = ViewDistance * 2;
+        Gizmos.color = Color.red;
+
+        // Draw view sphere to indicate the range
+        Gizmos.DrawWireSphere(transform.position, viewDiameter);
+
+        if (NumberOfDetectionLines == 1) {
+            DrawDetectionLines(0);
+        } else if (NumberOfDetectionLines % 2 == 0) { // EVEN number of detection lines
+            for (float i = -NumberOfDetectionLines / 2; i <= NumberOfDetectionLines / 2; i++) {
+                DrawDetectionLines(i);
+                Gizmos.DrawLine(transform.position, drawLineTo);
+            }
+        } else { // UNEVEN number of detection lines
+            for (float i = -NumberOfDetectionLines / 2 - .5f; i <= NumberOfDetectionLines / 2 + .5f; i++) {
+                DrawDetectionLines(i);
+                Gizmos.DrawLine(transform.position, drawLineTo);
+            }
+        }
+    }
+
+    private void DrawDetectionLines(float i) {
+        // Formula
+        float formula = i * ((ViewAngle * Mathf.Deg2Rad) / NumberOfDetectionLines);
+
+        float xPos = viewDiameter * Mathf.Cos(formula);
+        float yPos = viewDiameter * Mathf.Sin(formula);
+
+        // Create new line end position using the positions calculated above
+        drawLineTo = transform.position + new Vector3(xPos, 0, yPos);
+        Physics.Linecast(transform.position, drawLineTo, out RaycastHit hitInfo);
+        HitCheck(hitInfo);
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, drawLineTo);
+    }
+
+    private void HitCheck(RaycastHit hitInfo) {
         if (hitInfo.transform == null) {
             return;
         } else if (hitInfo.transform.name == "Player") {
@@ -18,6 +94,9 @@ public class Eye : MonoBehaviour {
         } else {
             Debug.Log("Hit " + hitInfo.transform.name);
         }
-        
+    }
+
+    private void LineVisualization() {
+
     }
 }
