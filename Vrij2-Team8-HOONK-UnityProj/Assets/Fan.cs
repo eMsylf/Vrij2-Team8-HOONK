d117a524fcd3f94af699fab94108a507b0ead343
Bob <bobjeltes@hotmail.com>
2019-05-19 17:05:39 +0200

    Fan Prototype
    First attempt at fan prototype. Wind strength decays over distance, and can be changed by altering the Main variable on the WindZone that is attached to the fan.

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/Fan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Fan : MonoBehaviour {
+    [SerializeField] private Collider WindHitbox;
+
+    void Awake() {
+        FindWindCollider();
+    }
+
+    void Update() {
+        
+    }
+
+    /// <summary>
+    /// Find a suitable wind collider that is a trigger among the Fan's children
+    /// </summary>
+    public void FindWindCollider () {
+        for (int i = 0; i < GetComponentsInChildren<Collider>().Length; i++) {
+            Collider tempCollider = GetComponentsInChildren<Collider>()[i];
+            if (tempCollider.isTrigger) {
+                WindHitbox = GetComponentsInChildren<Collider>()[i];
+            }
+        }
+
+        if (WindHitbox == null) {
+            Debug.LogWarning("There's no suitable collider to act as wind in the Fan's children. Make sure there's at least one collider set as Trigger present.");
+        }
+    }
+}
