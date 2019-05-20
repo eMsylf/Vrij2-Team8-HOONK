44361337f70a2d6affb6019f84ea4eae4bc3db59
Bob <bobjeltes@hotmail.com>
2019-05-20 15:04:53 +0200

    Update Fan.cs
    Slight fixes to formatting and overall functionality

--- Vrij2-Team8-HOONK-UnityProj/Assets/Fan.cs
+++ Vrij2-Team8-HOONK-UnityProj/Assets/Fan.cs
@@ -21,20 +21,22 @@ public class Fan : MonoBehaviour {
     void Awake() {
         if (GetComponent<WindZone>() == null) {
             gameObject.AddComponent<WindZone>();
+        } else {
+            windZone = GetComponent<WindZone>();
         }
         FindWindTrigger();
     }
 
     void FixedUpdate() {
-        currentStrength = Mathf.Sin(Time.time * strengthChangeSpeed) * (Mathf.Abs(fanStrengthMax - fanStrengthMin)/2) + Mathf.Abs((fanStrengthMax + fanStrengthMin) /2);
+        currentStrength = Mathf.Sin(Time.time * strengthChangeSpeed) * (Mathf.Abs(fanStrengthMax - fanStrengthMin) / 2) + Mathf.Abs((fanStrengthMax + fanStrengthMin) / 2);
         Debug.Log(currentStrength);
 
     }
 
     /// <summary>
-    /// Find a suitable wind collider that is a trigger among the Fan's children
+    /// Find a suitable wind collider that is a trigger among the Fan's children. This is done, assuming that there is only one trigger among the children
     /// </summary>
-    public void FindWindTrigger () {
+    public void FindWindTrigger() {
         for (int i = 0; i < GetComponentsInChildren<Collider>().Length; i++) {
             Collider tempCollider = GetComponentsInChildren<Collider>()[i];
             if (tempCollider.isTrigger) {
