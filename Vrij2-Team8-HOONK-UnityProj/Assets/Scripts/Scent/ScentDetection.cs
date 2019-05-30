734b7a7120fe83018ba1ace27aa01af9a9e8bdcd
Bob <bobjeltes@hotmail.com>
2019-05-30 20:46:29 +0200

    Ruikende robot
    Ik heb een ruikende robot. Hij ruikt erg goed. Te goed.

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/Scripts/Scent/ScentDetection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScentDetection : MonoBehaviour {
+
+    [Range(.01f, .1f)]
+    [SerializeField] private float movementSpeed = .01f;
+
+    private Transform destination;
+
+    private void Start() {
+        destination = transform;
+    }
+
+    private void FixedUpdate() {
+        if (transform != destination) {
+            Quaternion oldRotation = transform.rotation;
+
+            transform.position = Vector3.MoveTowards(transform.position, destination.position, movementSpeed);
+            transform.LookAt(destination);
+            // Keep x and z rotations at 0
+            Quaternion newRotation = transform.rotation;
+
+            transform.rotation = new Quaternion(0, newRotation.y, 0, newRotation.w);
+            
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.name.Contains("Scent particle pool")) {
+            destination = other.transform;
+            Debug.Log("Updated the destination position");
+        }
+        else if (other.name.Contains("Scent particle")) {
+            destination = other.transform;
+            Debug.Log("Updated the destination position");
+        }
+        else {
+            Debug.Log("<b>Nope that's not the right name</b>");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.name.Contains("Scent particle pool")) {
+            destination = transform;
+            Debug.Log("Updated the destination position to self");
+        }
+    }
+
+    private void MoveFromTo(Vector3 currentPosition, Vector3 destination) {
+        Vector3.Lerp(currentPosition, destination, movementSpeed);
+    }
+}
