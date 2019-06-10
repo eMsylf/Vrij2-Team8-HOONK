fe05d883e824ca7dd03de807240ce0f44e2ee464
Dirk <dirkdemeij@gmail.com>
2019-06-10 22:46:38 +0200

    Auto stash before merge of "pre-master-NEW" and "Art"

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/FmodSucks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMOD.Studio;
+using UnityEngine;
+
+public class FmodSucks : MonoBehaviour
+{
+    FMODEventTrack FMOD;
+    PARAMETER_ID partselector;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        EventInstance Part = FMODUnity.RuntimeManager.CreateInstance("event/event:/Background Music Ambience/SoundTrack");
+        Part.setProperty("PartSelector", 3f);
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
