ca3eaa137f6696de56e3184cffb7a269f5af540a
Dirk <dirkdemeij@gmail.com>
2019-06-10 22:47:00 +0200

    Auto stash before merge of "pre-master-NEW" and "Art"

--- Vrij2-Team8-HOONK-UnityProj/Assets/FmodSucks.cs
+++ Vrij2-Team8-HOONK-UnityProj/Assets/FmodSucks.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using FMOD.Studio;
+=======
+>>>>>>> Stashed changes
 using UnityEngine;
 
 public class FmodSucks : MonoBehaviour
 {
+<<<<<<< Updated upstream
     FMODEventTrack FMOD;
     PARAMETER_ID partselector;
     // Start is called before the first frame update
@@ -15,6 +19,21 @@ public class FmodSucks : MonoBehaviour
         Part.setProperty("PartSelector", 3f);
 
     }
+=======
+    public FMODEventMixerBehaviour flop;
+    private object Partselector;
+
+
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider collision)
+            {
+                if (collision.gameObject.name == "Player")
+                {
+            Partselector = 3;
+                }
+            }
+>>>>>>> Stashed changes
     
 
     // Update is called once per frame
