3134e8e88e5b43f36ac558b7defedf995bf89149
Bob Jeltes <bobjeltes@hotmail.com>
2019-06-11 01:04:07 +0200

    Sliding door & Robot rotatie detectie om de deur te openen
    Sliding door
    - De deur gaat openen wanneer alle aangekoppelde robots goed staan gedraaid
    - Robots kunnen aangekoppeld worden in de lijst "Required Robots" onder het Sliding Door script
    - In hetzelfde script kan worden aangegeven waar de deur heen moet schuiven, en hoe snel dat moet.
    
    Goed staan gedraaid
    - In het script "rotation check," dat aangekoppeld zit aan elke nieuwe robot, kan je een hoek bepalen waarin de robot moet staan om de deur te activeren. Elke hoek. Dit heeft uren geduurd maak er GEBRUIK VAN JULIA EN IK GA DE REST NOG FIXEN MAAR NU WERKT HET AAAAAAAAAAAAAAAAAAAAAAAAAAAA
    - De robots hebben een lichtje in de prefab om aan te geven dat de robot goed staat gedraaid.
    
    Robots en de sliding deur
    - Aangekoppelde robots zijn met een lijn tussen de deur en de robots aangegeven.

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/RotationCheck.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationCheck : MonoBehaviour {
+    public Light correctRotationLight;
+
+    public bool isRotatedCorrectly;
+    [Range(0f, 360f)]
+    [SerializeField] private float currentRotation;
+    [Range(0f, 360f)]
+    [SerializeField] private float rotationMin;
+    [Range(0f, 360f)]
+    [SerializeField] private float rotationMax;
+
+
+    private int heck = 5;
+    private float start;
+    private float partitions;
+
+    float hur;
+    float dur;
+
+    void Start() {
+        if (correctRotationLight == null) {
+            if (GetComponentInChildren<Light>() != null) {
+                correctRotationLight = GetComponentInChildren<Light>();
+            }
+        }
+        correctRotationLight.enabled = isRotatedCorrectly;
+    }
+
+    void Update() {
+        currentRotation = transform.eulerAngles.y;
+        if (currentRotation >= rotationMin && currentRotation <= rotationMax) {
+            isRotatedCorrectly = true;
+        } else {
+            isRotatedCorrectly = false;
+        }
+
+        correctRotationLight.enabled = isRotatedCorrectly;
+    }
+
+    private void OnDrawGizmosSelected() {
+        currentRotation = transform.eulerAngles.y;
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+
+        Gizmos.color = Color.cyan;
+
+        // Calculate the rotationMin position from the transform.forward and the rotationMin float
+
+        float rotationMinRad = Mathf.Deg2Rad * rotationMin;
+        float rotationMaxRad = Mathf.Deg2Rad * rotationMax;
+
+        Vector3 minPos = transform.position + new Vector3(Mathf.Sin(rotationMinRad), 0, Mathf.Cos(rotationMinRad)) * 2f;
+        Vector3 maxPos = transform.position + new Vector3(Mathf.Sin(rotationMaxRad), 0, Mathf.Cos(rotationMaxRad)) * 2f;
+
+        // Calculate points between the two lines
+
+        Gizmos.DrawLine(transform.position, minPos);
+        Gizmos.DrawLine(transform.position, maxPos);
+
+        /*
+        for (float i = start; i < partitions; i++) {
+            Vector3 tempPos1 = transform.position + new Vector3(Mathf.Sin((rotationMinRad + i - 1) / partitions), 0, Mathf.Cos((rotationMinRad + i - 1) / partitions)) * 2;
+            Vector3 tempPos2 = transform.position + new Vector3(Mathf.Sin((rotationMinRad + i) / partitions), 0, Mathf.Cos((rotationMinRad + i) / partitions)) * 2;
+        }
+        */
+
+        partitions = (rotationMaxRad - rotationMinRad);
+
+        Gizmos.color = Color.magenta;
+        for (float i = 0; i < partitions; i++) {
+            if (i == 0) {
+                hur = rotationMinRad + i;
+            } else {
+                hur = rotationMinRad + i - 1;
+            }
+            dur = rotationMinRad + i;
+
+            Vector3 tempPos1 = transform.position + new Vector3(Mathf.Sin(hur), 0, Mathf.Cos(hur)) * 2;
+            Vector3 tempPos2 = transform.position + new Vector3(Mathf.Sin(dur), 0, Mathf.Cos(dur)) * 2;
+            Gizmos.DrawLine(tempPos1, tempPos2);
+        }
+    }
+}
