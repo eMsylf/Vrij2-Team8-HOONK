d4901c1a221ba623e50ac88978334b627d170780
Dirk <dirkdemeij@gmail.com>
2019-06-11 01:50:45 +0200

    [+] tries for fmod

---
+++ Vrij2-Team8-HOONK-UnityProj/Assets/FmodSoundtrack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FmodSoundtrack : MonoBehaviour
+{
+
+    void Next(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
