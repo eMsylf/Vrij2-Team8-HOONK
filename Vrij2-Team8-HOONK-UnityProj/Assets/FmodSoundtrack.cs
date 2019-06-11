f663c6d3a7ab3f4ca43ddf78900142052bd74932
Dirk <dirkdemeij@gmail.com>
2019-06-11 02:28:27 +0200

    [+] whut someting worked with Fmod

--- Vrij2-Team8-HOONK-UnityProj/Assets/FmodSoundtrack.cs
+++ Vrij2-Team8-HOONK-UnityProj/Assets/FmodSoundtrack.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FmodSoundtrack : MonoBehaviour
-{
+{   public Button start;
+    public string path;
 
-    void Next(string path)
-    {
-        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
-    }
+
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        {
+            start.onClick.AddListener(delegate { Next(path); }); 
+        }
+    }
+    void Next(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
     }
 }
